2791d3df828768ac496cc3863019d72b1ffb3f35
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-01-23 17:39:17 +0500

    Select all necessary UI elements

---
+++ app.js
@@ -0,0 +1,6 @@
+// SELECT ALL UI ELEMENTS
+const count = document.querySelector(".count");
+const displayHeader = document.querySelector(".display-header");
+const resetBtn = document.querySelector(".btn-reset");
+const incrementBtn = document.querySelector(".btn-increment");
+const decrementBtn = document.querySelector(".btn-decrement");
