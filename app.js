f2db5818d1598bd3da6461a7a8e5e4c302478c17
Dersu <148701933+dersuprogg@users.noreply.github.com>
2024-01-23 17:45:07 +0500

    Add increment functionality

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // SELECT ALL UI ELEMENTS
-const count = document.querySelector(".count");
+const countHeader = document.querySelector(".count");
 const displayHeader = document.querySelector(".display-header");
 const resetBtn = document.querySelector(".btn-reset");
 const incrementBtn = document.querySelector(".btn-increment");
 const decrementBtn = document.querySelector(".btn-decrement");
+let count = 0;
+
+incrementBtn.addEventListener("click", () => {
+  count++;
+  countHeader.textContent = count;
+});
